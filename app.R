4531815a5f352244ce2ef525e8c5b2d8ba10aeb7
Eric Hare <ericrhare@gmail.com>
2017-12-12 09:54:19 -0600

    Change to date range input, add description

--- app.R
+++ app.R
@@ -13,27 +13,38 @@ addResourcePath("images", "images")
 
 ui <- fluidPage(theme = shinytheme("cerulean"),
    
-   # Application title
    titlePanel("Energy Usage Viewer"),
    
    sidebarLayout(
       sidebarPanel(
          a(href = "https://oaiti.org", target = "_blank", img(src = "images/oaiti_transparent.png", width = "135")),
+         
+         h4("About"),
+         HTML("This app allows the uploading of energy data in a particular format and performs an analysis of that data. The expected format is an Excel spreadsheet with columns <b>Date</b>, <b>Start Time</b>, <b>Duration</b>, <b>Value</b>, and <b>Year</b>. Once the data has been uploaded, the results will be computed and displayed automatically. The date range can be selected to subset the time frame considered."),
+         
+         hr(),
+         
+         h4("Configuration"),
          fileInput("elec", "Electricity Data"),
-         sliderInput("years",
-                     "Years:",
-                     min = 2015,
-                     max = 2017,
-                     value = c(2015, 2017),
-                     step = 1)
+         dateRangeInput("dates",
+                     "Date Range",
+                     start = "2015-11-09",
+                     end = "2017-10-30")
       ),
       
       mainPanel(
-        h4("Hourly Electricity Usage by Month"),
-        withSpinner(plotlyOutput("monthPlot")),
-        hr(),
-        h4("Hourly Electricity Usage by Day of Week"),
-        withSpinner(plotOutput("weekPlot"))
+          tabsetPanel(id = "tabs1",
+              tabPanel("Electricity",
+                       h4("Hourly Electricity Usage by Month"),
+                       withSpinner(plotlyOutput("monthPlot")),
+                       
+                       hr(),
+                       
+                       h4("Hourly Electricity Usage by Day of Week"),
+                       withSpinner(plotOutput("weekPlot"))
+              ),
+              tabPanel("Gas")
+          )
       )
    )
 )
@@ -42,15 +53,20 @@ server <- function(input, output, session) {
   
   observe({
     if (!is.null(cleaned_elec())) {
-      updateSliderInput(session, "years", min = min(year(cleaned_elec()$SDate)), max = max(year(cleaned_elec()$SDate)), value = c(min(year(cleaned_elec()$SDate)), max(year(cleaned_elec()$SDate))))
+      updateDateRangeInput(session, "dates", 
+                           min = min(cleaned_elec()$SDate), 
+                           max = max(cleaned_elec()$SDate), 
+                           start = min(cleaned_elec()$SDate), 
+                           end = max(cleaned_elec()$SDate))
     }
   })
    
    elec_data <- reactive({
-     if(is.null(input$elec))  return(NULL)
+     if (is.null(input$elec)) return(NULL)
      
      return(read_xlsx(input$elec$datapath))
    })
+   
    cleaned_elec <- reactive({
      if(is.null(elec_data())) return(NULL)
       elec_data() %>% 
@@ -65,8 +81,9 @@ server <- function(input, output, session) {
      if(is.null(cleaned_elec())) return(NULL)
      
      cleaned_elec() %>%
-      filter(Year %in% input$years)
+      filter(SDate >= input$dates[1], SDate <= input$dates[2])
    })
+   
    output$monthPlot <- renderPlotly({
      if(is.null(cleaned_elec())) return(NULL)
      
@@ -76,6 +93,7 @@ server <- function(input, output, session) {
      
      ggplotly(g, tooltip = "text")
    })
+   
    output$weekPlot <- renderPlot({
      if(is.null(cleaned_elec())) return(NULL)
      
@@ -85,5 +103,4 @@ server <- function(input, output, session) {
   })
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
