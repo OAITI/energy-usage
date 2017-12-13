ac3a21ff26505a09cd1fe755e14b60198e760ab5
Eric Hare <ericrhare@gmail.com>
2017-12-13 11:24:52 -0600

    Specify aggregation level

--- app.R
+++ app.R
@@ -25,23 +25,26 @@ ui <- fluidPage(theme = shinytheme("cerulean"),
          hr(),
          
          h4("Configuration"),
-         fileInput("elec", "Electricity Data"),
-         dateRangeInput("dates",
-                     "Date Range",
-                     start = "2015-11-09",
-                     end = "2017-10-30")
+         conditionalPanel(condition = "input.tabs1 == 'Electricity'",
+                          fileInput("elec", "Electricity Data", accept = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"),
+                          selectInput("aggregation", "Aggregation Level", choices = c("Hourly" = "Hour", "Daily" = "Day", "Weekly" = "Week"), selected = "Daily"),
+                          dateRangeInput("dates",
+                                         "Date Range",
+                                         start = "2015-11-09",
+                                         end = "2017-10-30")
+         )
       ),
       
       mainPanel(
           tabsetPanel(id = "tabs1",
               tabPanel("Electricity",
-                       h4("Hourly Electricity Usage by Month"),
-                       withSpinner(plotlyOutput("monthPlot")),
+                       h4("Electricity Usage by Month"),
+                       withSpinner(plotlyOutput("monthPlot", height = "600px")),
                        
                        hr(),
                        
-                       h4("Hourly Electricity Usage by Day of Week"),
-                       withSpinner(plotOutput("weekPlot"))
+                       h4("Electricity Usage by Day of Week"),
+                       withSpinner(plotlyOutput("weekPlot", height = "600px"))
               ),
               tabPanel("Gas")
           )
@@ -54,12 +57,12 @@ server <- function(input, output, session) {
   observe({
     if (!is.null(cleaned_elec())) {
       updateDateRangeInput(session, "dates", 
-                           min = min(cleaned_elec()$SDate), 
-                           max = max(cleaned_elec()$SDate), 
-                           start = min(cleaned_elec()$SDate), 
-                           end = max(cleaned_elec()$SDate))
+                           min = min(as.Date(cleaned_elec()$SDate)), 
+                           max = max(as.Date(cleaned_elec()$SDate)), 
+                           start = min(as.Date(cleaned_elec()$SDate)), 
+                           end = max(as.Date(cleaned_elec()$SDate)))
     }
-  })
+  }, suspended = TRUE)
    
    elec_data <- reactive({
      if (is.null(input$elec)) return(NULL)
@@ -68,38 +71,59 @@ server <- function(input, output, session) {
    })
    
    cleaned_elec <- reactive({
-     if(is.null(elec_data())) return(NULL)
-      elec_data() %>% 
-        rename(hourly_value = Value) %>%
-        mutate(SDate=as.Date(Date),Year=as.factor(Year)) %>%
-        mutate(Month=month(SDate, label = T)) %>% 
-        mutate(Day=wday(SDate, label=TRUE))
-     
+     if (is.null(elec_data())) return(NULL)
+       
+        mygroup <- c("Year", "Month", "WeekDay", input$aggregation)
+       
+        elec_data() %>% 
+            select(-Year) %>%
+            mutate(SDate = ymd_h(paste(Date, `Start Time` / 100, sep = "-")),
+                   Hour = hour(SDate),
+                   Day = yday(SDate),
+                   Week = week(SDate),
+                   WeekDay = wday(SDate, label = TRUE),
+                   Month = month(SDate, label = TRUE),
+                   Year = year(SDate)) %>%
+            group_by_at(vars(one_of(mygroup))) %>%
+            summarise(Value = sum(Value),
+                      SDate = SDate[1])
    })
    
    filtered_clean_elec <- reactive({
-     if(is.null(cleaned_elec())) return(NULL)
-     
+     if (is.null(cleaned_elec())) return(NULL)
+       
      cleaned_elec() %>%
-      filter(SDate >= input$dates[1], SDate <= input$dates[2])
+        filter(SDate >= input$dates[1], SDate <= input$dates[2])
+   })
+   
+   agg_elec <- reactive({
+       if (is.null(filtered_clean_elec())) return(NULL)
+       
+       mydat <- filtered_clean_elec()
+       mydat$myvar <- unlist(mydat[,input$aggregation])
+       
+       return(mydat)
    })
    
    output$monthPlot <- renderPlotly({
      if(is.null(cleaned_elec())) return(NULL)
-     
-     g <- ggplot(data=filtered_clean_elec(),aes(x=`Start Time`, y=hourly_value, group=`Start Time`, text=SDate)) +
-       geom_point( size=0.3) + facet_wrap(~Month, scales = "free_x") +
+           
+     g <- ggplot(data=agg_elec(),aes(x=myvar, y=Value, group=myvar, text=SDate)) +
+         xlab(input$aggregation) +
+       geom_point(size = 0.3) + facet_wrap(~Month, scales = "free_x") +
        theme_bw()
      
      ggplotly(g, tooltip = "text")
    })
    
-   output$weekPlot <- renderPlot({
+   output$weekPlot <- renderPlotly({
      if(is.null(cleaned_elec())) return(NULL)
      
-      ggplot(data=filtered_clean_elec(),aes(x=`Start Time`, y=hourly_value, group=`Start Time`, text=SDate)) +
-        geom_boxplot() + facet_wrap(~Day, scales = "free_x") +
-        theme_bw()
+       g <- ggplot(data=agg_elec(),aes(x=myvar, y=Value, group=myvar, text=SDate)) +
+                    xlab(input$aggregation) +
+                    theme_bw() + geom_point(size = 0.3) + facet_wrap(~WeekDay, scales = "free_x")
+       
+       ggplotly(g, tooltip = "text")
   })
 }
 
