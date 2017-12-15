6e455ed6816f71597e5d0be20485046dc8735e65
Nadi90 <nadi1990@gmail.com>
2017-12-15 10:11:19 -0600

    Revert "Updated tooltip"
    This reverts commit 162148d8749e005e0c40902ca90dbbd402a0cfb9.

--- app.R
+++ app.R
@@ -162,7 +162,7 @@ server <- function(input, output, session) {
            mydat$Date <- ymd(paste(year(mydat$Date), "01", "01", sep = "-"))
        }
        
-       g <- ggplot(data = mydat, aes(x = Date, y = Usage, group = 1, text = paste('Date:', format(Date, "%b %Y")))) +
+       g <- ggplot(data = mydat, aes(x = Date, y = Usage, group = 1)) +
            geom_line() +
            theme_bw() +
            theme(axis.text.x = element_text(angle = 45, size = 8))
@@ -175,7 +175,7 @@ server <- function(input, output, session) {
        
        if (input$aggregation != "Daily") g <- g + geom_point()
        
-       ggplotly(g, tooltip = c("Usage", "text"))
+       ggplotly(g, tooltip = c("Usage", "Date"))
    })
    
    elec_grouped <- reactive({
@@ -237,7 +237,7 @@ server <- function(input, output, session) {
    output$gas_time_series <- renderPlotly({
        if (is.null(gas_series())) return(NULL)
        
-       g <- ggplot(data = gas_series(), aes(x = Date, y = Usage, group = 1, text = paste('Date:', format(Date, "%b %Y")))) +
+       g <- ggplot(data = gas_series(), aes(x = Date, y = Usage, group = 1)) +
            geom_point() +
            geom_line() +
            theme_bw() +
@@ -249,7 +249,7 @@ server <- function(input, output, session) {
            g <- g + scale_x_date(date_breaks = "3 months", date_minor_breaks = "1 month", date_labels = "%b %Y")
        }
        
-       ggplotly(g, tooltip = c("Usage", "text"))
+       ggplotly(g, tooltip = c("Usage", "Date"))
    })
    
    temperature_data <- reactive({
